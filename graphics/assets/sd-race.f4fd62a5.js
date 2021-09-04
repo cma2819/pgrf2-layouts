9d7c20d84fc865869ed2cc3a8ab9d4e2b5a197e8
Cma <chicchaima@gmail.com>
2021-09-04 10:56:14 +0900

    fix a bit

---
+++ graphics/assets/sd-race.f4fd62a5.js
@@ -0,0 +1,1 @@
+import{i as o}from"./vendor.82ab3a02.js";import{_ as r}from"./SDRace.afe93080.js";import{s as a}from"./index.eeba7876.js";import"./Footer.d6e861c6.js";import"./Split.baf9a69b.js";o(r).use(a).mount("#root");
